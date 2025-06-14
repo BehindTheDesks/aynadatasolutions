9785339bed7089cd70ef0ef5348b32208fcfd684
Ekeocha-ibe victor chinemerem <victorekeocha016@gmail.com>
2025-06-14 08:11:51 +0100

    added another prod route

--- src/hooks/helper.ts
+++ src/hooks/helper.ts
@@ -1,4 +1,4 @@
 export const isProduction = (): boolean => {
-    const productionHosts = ["aynadatasolutions.com"];
+    const productionHosts = ["aynadatasolutions.com", "www.aynadatasolutions.com"];
     return productionHosts.includes(window.location.hostname);
 };
